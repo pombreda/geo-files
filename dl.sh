ab5b89629917a5308614a2527c3ebef5bcade2b4
Ramiro Gómez <code@ramiro.org>
2013-10-17 01:11:22 +0200

    use lower resolution

--- dl.sh
+++ dl.sh
@@ -2,4 +2,5 @@
 cd shp
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip
 wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
 find . -name '*.zip' -exec unzip {} \;
--- mksvg.py
+++ mksvg.py
@@ -8,17 +8,18 @@ gc = GeonamesCache()
 countries = gc.get_countries()
 
 for iso2 in list(countries.keys()):
-#for iso2 in ['FR']:
+#for iso2 in ['FR', 'ES']:
     cfg = {
         'layers': [{
             'id': 'country',
-            'src': 'shp/ne_10m_admin_0_countries.shp',
-            'filter': {'ISO_A2': iso2},
+            'src': 'shp/ne_50m_admin_0_countries.shp',
+#            'src': 'shp/ne_10m_admin_1_states_provinces.shp',
+            'filter': {'iso_a2': iso2},
         }]
     }
     svg = 'svg/%s.svg' % iso2
     try:
-        K.generate(cfg, outfile=svg, stylesheet='#country {fill: black;}')
+        K.generate(cfg, outfile=svg, stylesheet='#country {fill: #000;}')
     except Exception as err:
         print(
             ('Exception for country %s:\n%r' % (countries[iso2]['name'], err)))
